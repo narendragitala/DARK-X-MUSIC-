4df52fed55f704cdc2088b87f1bed50009914105
RADHEEXUSER <157900964+RADHEEXUSER@users.noreply.github.com>
2024-02-08 16:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -111,13 +111,13 @@ DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
 if SUPPORT_CHANNEL:
-    if not re.match("(https://t.me/LOVE_BIRDS_KI_DUNIYA)://", SUPPORT_CHANNEL):
+    if not re.match("()://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
 
 if SUPPORT_CHAT:
-    if not re.match("(https://t.me/LOVE_BIRDS_KI_DUNIYA)://", SUPPORT_CHAT):
+    if not re.match("()://", SUPPORT_CHAT):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
         )
