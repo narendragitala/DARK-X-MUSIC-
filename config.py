6fa63c90d158a65abd889370813a11e1800dcb3d
RADHEEXUSER <157900964+RADHEEXUSER@users.noreply.github.com>
2024-02-14 10:26:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RADHEEXUSER/bikk",
+    "https://github.com/RADHEEXUSER/Radhe-ki-nagri-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
