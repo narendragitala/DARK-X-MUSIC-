d40b5e68706eb7a1839e5612e3c18175c702d8b5
narendragitala <151159508+narendragitala@users.noreply.github.com>
2024-03-04 08:57:24 +0530

    𝗡𝗮𝗿𝘂 𝗷𝗮𝗮𝘁

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","@mr_naru")
+OWNER_USERNAME = getenv("OWNER_USERNAME","mr_naru")
 # Get Your bot username
 BOT_USERNAME = getenv("BOT_USERNAME" , "DaisyyMusixBot")
 # Don't Add style font 
@@ -26,10 +26,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002017835000))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002063300169))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6927005356))
+OWNER_ID = int(getenv("OWNER_ID", 5473771943))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
